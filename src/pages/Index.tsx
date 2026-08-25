2bcc3cc6385c5951a16711e79b5d22fa9e00eab7
caiobas <caioemanoel36@gmail.com>
2026-08-25 14:36:59 -0300

    feat: adiciona as 3 opcoes de design do ValueSection com icones refinados

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -8,13 +8,19 @@ import IdentitySection from "@/components/MVVSection";
 import ServicesSection from "@/components/ServicesSection";
 import Footer from "@/components/Footer";
 import WhatsAppButton from "@/components/WhatsAppButton";
+import ValueSectionBento from "@/components/ValueSection_Bento";
+import ValueSectionEditorial from "@/components/ValueSection_Editorial";
+import ValueSectionIndustrial from "@/components/ValueSection_Industrial";
 
 const Index = () => {
     return (
         <div className="min-h-screen">
             <Header />
             <HeroSection />
-            <ValueSection />
+            {/* <ValueSection /> */}
+            {/* <ValueSectionBento/> */}
+            {/* <ValueSectionEditorial/> */}
+            <ValueSectionIndustrial/>
             <AboutSection />
             <IdentitySection />
             <ServicesSection />
